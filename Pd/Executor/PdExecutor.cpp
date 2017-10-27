973b1c49db50053a025aef91622c68f629a476a1
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-10-27 09:34:56 +0200

    remove debug

--- Pd/Executor/PdExecutor.cpp
+++ Pd/Executor/PdExecutor.cpp
@@ -363,7 +363,6 @@ void PdGraphNode::run(ossia::token_request t, ossia::execution_state& e)
     auto mess = m_inmess[i].c_str();
     for(const auto& val : dat)
     {
-      qDebug() << val;
       val.apply(ossia_to_pd_value{mess});
     }
     dat.clear();
